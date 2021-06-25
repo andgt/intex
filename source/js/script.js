b0a9579907eeb237ee820edae09c09381758c137
Anatoly <anatolijdegtyar@yandex.ru>
2021-06-25 16:23:27 +0300

    Стилизует футер

--- source/js/script.js
+++ source/js/script.js
@@ -107,3 +107,19 @@ btnLeft.onclick = function() {
 btnRight.onclick = function() {
   nextSlide(index);
 }
+
+// Кнопка наверх
+
+let scrollUp = document.querySelector(".button__scroll-up");
+
+window.onscroll = function () {
+  if (window.pageYOffset > 100) {
+    scrollUp.classList.add("button__scroll-up--showed");
+  } else {
+    scrollUp.classList.remove("button__scroll-up--showed");
+  }
+};
+
+scrollUp.onclick = function (evt) {
+  window.scrollTo(0, 0);
+};
