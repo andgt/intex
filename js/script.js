23eb66b5eb3d671f9cc5d81b591511b2951d892a
Anatoly <anatolijdegtyar@yandex.ru>
2021-06-28 17:17:38 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -153,43 +153,46 @@ window.addEventListener("resize", function() {
   }
 });
 
-// Кнопка наверх
-
-let scrollUp = document.querySelector(".button__scroll-up");
-
-window.onscroll = function () {
-  if (window.pageYOffset > 100) {
-    scrollUp.classList.add("button__scroll-up--showed");
-  } else {
-    scrollUp.classList.remove("button__scroll-up--showed");
-  }
-};
-
-scrollUp.onclick = function (evt) {
-  window.scrollTo(0, 0);
-};
-
 
 // Фиксированное меню
 
-let logo = document.querySelector(".header__logo");
+let menuFixed = function() {
+  let logo = document.querySelector(".header__logo");
 
-window.onscroll = function () {
   if (window.pageYOffset > 10) {
     mainNav.classList.add("main-nav__sticky");
   } else {
     mainNav.classList.remove("main-nav__sticky");
   }
-}
 
-if (window.innerWidth < 768) {
-  window.onscroll = function () {
+  if (window.innerWidth < 768) {
     if (window.pageYOffset > 10) {
       mainNav.classList.add("main-nav__sticky");
       logo.classList.add("header__logo-sticky");
     } else {
       mainNav.classList.remove("main-nav__sticky");
       logo.classList.remove("header__logo-sticky");
-    }
+    };
   }
-}
+};
+
+// Кнопка наверх
+
+let scrollUpButton = function () {
+  let scrollUp = document.querySelector(".button__scroll-up");
+
+  if (window.pageYOffset > 100) {
+    scrollUp.classList.add("button__scroll-up--showed");
+  } else {
+    scrollUp.classList.remove("button__scroll-up--showed");
+  }
+
+  scrollUp.onclick = function (evt) {
+    window.scrollTo(0, 0);
+  };
+};
+
+window.onscroll = function() {
+  menuFixed();
+  scrollUpButton();
+};
