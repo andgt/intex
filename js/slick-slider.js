06c697546fab3292c98aac6e9d26651d8c1e5e47
Anatoly <anatolijdegtyar@yandex.ru>
2021-07-07 13:26:09 +0300

    Updates

---
+++ js/slick-slider.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+  $('.slick-slider--big').slick({
+    arrows: false,
+    speed: 1000,
+    initialSlide: 1,
+    fade: true,
+    asNavFor: ".slick-slider--min"
+  });
+});
+
+$(document).ready(function(){
+  $('.slick-slider--min').slick({
+    arrows: true,
+    slidesToShow: 3,
+    speed: 1000,
+    initialSlide: 1,
+    centerMode: true,
+    variableWidth: true,
+    asNavFor: ".slick-slider--big",
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
+  });
+});
